8aa397d5ce21a39f4f39b625f8b692eaf1406e0d
DEADPOOL712 <98164944+DEADPOOL712@users.noreply.github.com>
2023-03-06 23:46:08 +0530

    ui change/convert to node

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const ejs = require("ejs");
+const PubertyRoute = require("./Routes/puberty");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use("/puberty", PubertyRoute);
+
+app.get("/", (req, res) => {
+  res.render("Home");
+});
+app.get("/explore", (req, res) => {
+  res.render("explore");
+});
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.listen(3000, () => {
+  console.log("Server is live !");
+});
