de127d422e43984bf48dd1cbb569fb9707d737ca
DEADPOOL712 <98164944+DEADPOOL712@users.noreply.github.com>
2023-03-11 18:21:00 +0530

    change contac,explore,add new routes

--- server.js
+++ server.js
@@ -3,10 +3,20 @@ const app = express();
 const ejs = require("ejs");
 const PubertyRoute = require("./Routes/puberty");
 const HormonesRoute = require("./Routes/hormones");
+const EducationRoute = require("./Routes/education");
+const MenstruationRoute = require("./Routes/menstruation");
+const PreventionRoute = require("./Routes/prevention-cure");
+const PrideRoute = require("./Routes/pride-flag");
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
 app.use("/puberty", PubertyRoute);
 app.use("/hormones", HormonesRoute);
+app.use("/education", EducationRoute);
+app.use("/menstruation", MenstruationRoute);
+app.use("/prevention-cure", PreventionRoute);
+app.use("/pride-flag", PrideRoute);
 
 app.get("/", (req, res) => {
   res.render("home");
